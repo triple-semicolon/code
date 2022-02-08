397b5bdc727ef093c1573aea347b9bb9fcc8f3c0
aslla3325 <72788533+aslla3325@users.noreply.github.com>
2022-02-08 13:21:49 +0900

    잡담 기능 추가 java 파일

---
+++ 220208/p0208/src/banana/RecomAVO.java
@@ -0,0 +1,25 @@
+package banana;
+
+public class RecomAVO {
+	public Integer getAns_no() {
+		return ans_no;
+	}
+	public void setAns_no(Integer ans_no) {
+		this.ans_no = ans_no;
+	}
+	public Integer getNo() {
+		return no;
+	}
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	private Integer ans_no = null;
+	private Integer no = null;
+	private String username = null;
+}
