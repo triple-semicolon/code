2758c9acb3183acdb4aadc69ef44ac44f186f99d
choiyezz <97445004+choiyezz@users.noreply.github.com>
2022-02-08 14:31:12 +0900

    Delete 220208/p0208 directory

--- 220208/p0208/src/banana/LoginDAO.java
+++
@@ -1,7 +0,0 @@
-package banana;
-
-public interface LoginDAO {
-	public LoginVO findBy_Id( String id ) throws Exception;
-	public LoginVO findBy_IdPwd( String id, String pwd ) throws Exception;
-	public String add_join(String id, String pwd) throws Exception;
-}
